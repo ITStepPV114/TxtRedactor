053f5464ab28dc4a3b0072dbf76d0a01b10b81cf
vovap <vovap@DESKTOP-PVPO5ME>
2023-05-12 19:48:17 +0300

    All works done

--- TextRedactor/MainWindow.xaml.cs
+++ TextRedactor/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -30,11 +31,32 @@ namespace TextRedactor
             };
             var menuControl = new MenuControl(rtbEditor);
             mainPanel.Children.Add(menuControl);
+            rtbEditor.Document.LineHeight = 3;
+            //BlockCollection rtbBc = rtbEditor.Document.Blocks;
+            //foreach(var block in rtbBc) 
+            //{
+            //    block.TextAlignment= TextAlignment.Center;
+            //}
+            //Paragraph p = rtbEditor.Document.Blocks.FirstBlock as Paragraph;
+            //p.LineHeight = 3;
         }
 
-        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        private void rtbEditor_TextChanged(object sender, TextChangedEventArgs e)
         {
+            string fileText = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd).Text;
+            int lineCount = 0;
+            int charCount = 0;
+            for (int i = 0; i < fileText.Length; i++)
+            {
+                if (fileText[i] == '\n')
+                {
+                    lineCount++;
+                }
+            }
 
+            charCount = fileText.Length;
+
+            statusText.Text = $"Lines: {lineCount}, Chars: {charCount}";
         }
     }
 }
